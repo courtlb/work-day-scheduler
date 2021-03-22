4b4ec9a7c75c8baba4e3c9f2b0c2f150f7f5a485
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-03-21 21:22:05 -0500

    formatting in js file and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Today's date in jumbotron
 var jumboDate = moment().format('dddd, MMMM Do');
 var scheduleContainer = $(".container");
 
@@ -6,7 +7,7 @@ var dateEl = $("<p>")
     .text(jumboDate);
 $(".jumbotron").append(dateEl);
 
-
+//color code time blocks
 var auditTime = function() {
     $(".time-block").each(function() {
         var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
@@ -26,6 +27,7 @@ var auditTime = function() {
     })
 };
 
+//get saved information, if any, from local storage
 var loadSchedule = function() {
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
@@ -41,15 +43,22 @@ var loadSchedule = function() {
     auditTime();
 };
 
+// when save button is clicked, set the description and time to local storage
 $(".saveBtn").on("click", function() {
-    var schedDescr = $(this).siblings(".description").val();
-    var schedTime = $(this).parent().attr("id");
+    var schedDescr = $(this)
+        .siblings(".description")
+        .val();
+    var schedTime = $(this)
+        .parent()
+        .attr("id");
+
     console.log(schedDescr);
     console.log(schedTime);
     
     localStorage.setItem(schedTime, schedDescr);
 });
 
+//call function that color codes every 15min, so you don't have to refresh the page
 setInterval(function() {
     $(".time-block").each(function(index, el) {
       auditTime(el);
