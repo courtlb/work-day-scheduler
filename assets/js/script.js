75a76153137fb03c3cad1a1525c7151c248204e4
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-03-21 21:15:43 -0500

    local storage, loadSchedule, saving schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 var jumboDate = moment().format('dddd, MMMM Do');
+var scheduleContainer = $(".container");
+
 var dateEl = $("<p>")
     .addClass("lead")
     .text(jumboDate);
 $(".jumbotron").append(dateEl);
+
 
 var auditTime = function() {
     $(".time-block").each(function() {
@@ -23,4 +26,34 @@ var auditTime = function() {
     })
 };
 
-auditTime();
+var loadSchedule = function() {
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    auditTime();
+};
+
+$(".saveBtn").on("click", function() {
+    var schedDescr = $(this).siblings(".description").val();
+    var schedTime = $(this).parent().attr("id");
+    console.log(schedDescr);
+    console.log(schedTime);
+    
+    localStorage.setItem(schedTime, schedDescr);
+});
+
+setInterval(function() {
+    $(".time-block").each(function(index, el) {
+      auditTime(el);
+    });
+  }, (1000 * 60) * 15);
+
+  loadSchedule();
